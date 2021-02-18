2434b2f7775afc7c346518fb232b2d3db18ca5b4
Oleg Shestakov <olshec@gmail.com>
2021-02-18 18:18:57 +0300

    added show and hide image by click ESC

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,8 @@
 const DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
 const DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
 const THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+const HIDDEN_DETAIL_CLASS = 'hidden-detail';
+const ESC_KEY = 27;
 
 function setDetails(imageUrl, titleText) {
 	let detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR); 
@@ -27,6 +29,7 @@ function addThumbClickHandler(thumb) {
 	thumb.addEventListener('click', function(event) {
 		event.preventDefault();
 		setDetailsFromThumb(thumb);
+		showDetails();
 	});
 }
 
@@ -36,9 +39,28 @@ function getThumbnailsArray() {
 	return thumbnailArray;
 }
 
+function hideDetails() {
+	document.body.classList.add(HIDDEN_DETAIL_CLASS);
+}
+
+function showDetails() {
+	document.body.classList.remove(HIDDEN_DETAIL_CLASS);
+}
+
+function addKeyPressHandler() {
+	document.body.addEventListener('keyup', function(event) {
+		event.preventDefault();
+		console.log(event.keyCode);
+		if (event.keyCode === ESC_KEY) {
+			hideDetails();
+		}
+	});
+}
+
 function initializeEvents() {
 	let thumbnails = getThumbnailsArray();
 	thumbnails.forEach(addThumbClickHandler);
+	addKeyPressHandler();
 }
 
 initializeEvents();
